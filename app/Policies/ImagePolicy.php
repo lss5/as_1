9544dc5371b2ffb8dcc40938a236734d884ba9d0
lss5 <serstel@gmail.com>
2022-01-25 19:40:11 +0600

    clear code Policies

--- app/Policies/ImagePolicy.php
+++ app/Policies/ImagePolicy.php
@@ -10,85 +10,38 @@ class ImagePolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function viewAny(User $user)
     {
-        //
+        return true;
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Image  $image
-     * @return mixed
-     */
     public function view(User $user, Image $image)
     {
-        //
+        return true;
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function create(User $user)
     {
-        //
+        return $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Image  $image
-     * @return mixed
-     */
     public function update(User $user, Image $image)
     {
-        //
+        return ($user->id == $image->product->user_id || $user->hasRole('admin'));
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Image  $image
-     * @return mixed
-     */
     public function delete(User $user, Image $image)
     {
-        //
+        return ($user->id == $image->product->user_id || $user->hasRole('admin'));
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Image  $image
-     * @return mixed
-     */
     public function restore(User $user, Image $image)
     {
-        //
+        return ($user->id == $image->product->user_id || $user->hasRole('admin'));
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Image  $image
-     * @return mixed
-     */
     public function forceDelete(User $user, Image $image)
     {
-        //
+        return $user->hasRole('admin');
     }
 }
--- app/Policies/ProductPolicy.php
+++ app/Policies/ProductPolicy.php
@@ -10,85 +10,38 @@ class ProductPolicy
 {
     use HandlesAuthorization;
 
-    /**
-     * Determine whether the user can view any models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function viewAny(User $user)
     {
         return $user->hasAnyRoles(['admin', 'moder']);
     }
 
-    /**
-     * Determine whether the user can view the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Product  $product
-     * @return mixed
-     */
     public function view(User $user, Product $product)
     {
         return $user->hasAnyRoles(['admin', 'moder']);
     }
 
-    /**
-     * Determine whether the user can create models.
-     *
-     * @param  \App\User  $user
-     * @return mixed
-     */
     public function create(User $user)
     {
-        //
+        return $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can update the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Product  $product
-     * @return mixed
-     */
     public function update(User $user, Product $product)
     {
         return ($user->id == $product->user_id || $user->hasRole('admin'));
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Product  $product
-     * @return mixed
-     */
     public function delete(User $user, Product $product)
     {
         return ($user->id == $product->user_id || $user->hasRole('admin'));
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Product  $product
-     * @return mixed
-     */
     public function restore(User $user, Product $product)
     {
-        //
+        return $user->hasRole('admin');
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     *
-     * @param  \App\User  $user
-     * @param  \App\Product  $product
-     * @return mixed
-     */
     public function forceDelete(User $user, Product $product)
     {
-        //
+        return $user->hasRole('admin');
     }
 }
